9cb1f7527cc0845cdc397cdd76b1f00a40f2f01e
Nikiva Davis <Nikivadavis@gmail.com>
2025-06-30 18:42:00 -0400

    user schema

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express")
+const mongoose = require("mongoose");
+const user = require("./models/user")
+
 const app = express()
 const PORT = process.env.PORT || 4000
+const{dbconnect} = require("./db")
 
 app.listen(PORT, () => {
 	console.log(`[server] listening on ${PORT}`)
 })
+
+
